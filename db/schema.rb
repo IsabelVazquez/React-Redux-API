a62272ec411c2991fd50a168d41160a45fb755b1
github username <12ivazquez@gmail.com>
2017-12-29 12:05:09 -0600

    removed JWT file and cleared database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171130151544) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string "imgur_id"
     t.integer "user_id"
