35792a70182c52745db7b8828d5e24b89e646077
github username <12ivazquez@gmail.com>
2017-12-28 19:10:42 -0600

    modified cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://imgurapi1.herokuapp.com'
+    origins 'localhost:3000', 'https://imgurreact.herokuapp.com'
 
     resource '*',
       headers: :any,
